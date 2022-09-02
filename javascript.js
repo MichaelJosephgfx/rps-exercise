71bc3ebbbc886deaee3bc311cdc723ab3872018b
Michael Joseph <michaeljosephgfx@gmail.com>
2022-09-01 20:46:05 -0400

    complete computer response

--- javascript.js
+++ javascript.js
@@ -1,24 +1,15 @@
 //RPS Exercise
 
-/*
-function getComputerChoice(Rock, Paper, Scissors) {
-    let comp = ('Rock', 'Paper', 'Scissors')
-    return Math.floor(Math.random()*3) + 1;
-}
-
-getComputerChoice(); */
-
-
-
+//Computer's Choice
 
 function getComputerChoice(Rock, Paper, Scissors) {
     let integer = '';
-    Math.floor(Math.random()*3) + 1;
-    if (integer > 1) {
-        return 'rock'
+    integer = Math.floor(Math.random()*3) + 1;
+    if (integer === 1) {
+        return 'rock';
     }
-    else if (integer > 2) {
-        return 'paper'
+    else if (integer === 2) {
+        return 'paper';
     }
     else {
         return 'scissors';
