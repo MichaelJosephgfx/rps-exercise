a1aa0995702992f4d6f038413d93dc36bfb12fe4
Michael Joseph <michaeljosephgfx@gmail.com>
2022-09-06 23:33:20 -0400

    complete round function

--- javascript.js
+++ javascript.js
@@ -21,7 +21,20 @@ function getComputerChoice(Rock, Paper, Scissors) {
 //Player's Choice........................................
 
 function playRound (playerSelection, computerSelection) {
-    return 'You won! Paper beats rock.'
+
+    if (playerSelection === computerSelection) {
+        return `Tie! You both picked ${playerSelection}`
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats scissors.';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats rock.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beats paper.';
+    } else {  
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
 }
-const playerSelection = "rock";
+const playerSelection = prompt('Make your choice: rock, paper, or scissors.').toLowerCase();
 const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
