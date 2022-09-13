668194f8337067ba6bef8bf01ea43644e0eee779
Michael Joseph <michaeljosephgfx@gmail.com>
2022-09-12 22:14:19 -0400

    refine score function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,23 @@
 //RPS Exercise
 
+// GLOBAL SCOPE VARIABLES
+
+
+let playerWins = "You won this round!";
+let compWins = "You lost, the computer won this one!";
+let tie = "It's a tie!";
+let compDefeat = "You won the game!";
+let playerDefeat = "You lost the game!";
+
+
 //Computer's Choice................................
 
 function getComputerChoice() {
-    let integer = Math.floor(Math.random()*3) + 1;
-    if (integer === 1) {
+    let rando = Math.floor(Math.random()*3) + 1;
+    if (rando === 1) {
         return 'rock'.toLowerCase();
     }
-    else if (integer === 2) {
+    else if (rando === 2) {
         return 'paper'.toLowerCase();
     }
     else {
@@ -15,6 +25,9 @@ function getComputerChoice() {
     }
 }
 
+let playerSelection = prompt('Make your choice: rock, paper, or scissors.').toLowerCase();
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 //Player's Choice........................................
 
@@ -31,35 +44,38 @@ function playRound (playerSelection, computerSelection) {
     } else {  
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
-    
+} 
+
+
+function winner() {
+    playRound(playerSelection, computerSelection)
+    if (playerSelection > computerSelection) {
+        return `player scored! ${++playerScore}`;
+    }
+    else if (computerSelection > playerSelection) {
+        return `comp scored! ${++compScore}`;
+    }
+    else {
+        return "Seems to be a tie";
+    }
 }
-
-// GLOBAL SCOPE VARIABLES
-
-let playerSelection = prompt('Make your choice: rock, paper, or scissors.').toLowerCase();
-let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-let playerWins = "You won this round!";
-let compWins = "You lost, the computer won this one!";
-let tie = "It's a tie!";
-let compDefeat = "You won the game!";
-let playerDefeat = "You lost the game!";
-
-let gameResult = playRound();
-    
+let playerScore = 0;
+let compScore = 0;
+console.log(winner(playerSelection, computerSelection));
 //GAME.......................................................
 
 function game() {
+
     for (let i = 1; i < 5; i++) {
         console.log(`Round number: ${i}`);
         let playerSelection = prompt('Make your choice: rock, paper, or scissors.').toLowerCase();
         let computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
-        playRound(playerSelection, computerSelection);   
+        playRound(playerSelection, computerSelection);  
+        winner(playerSelection, computerSelection); 
     }
-    
 }
+console.log(game());
 
-game();
+
 
