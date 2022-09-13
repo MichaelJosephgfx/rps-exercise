f53b54cdc529632ced9d00930f5b5fc5d528629b
Michael Joseph <michaeljosephgfx@gmail.com>
2022-09-13 01:12:51 -0400

    score funtion working

--- javascript.js
+++ javascript.js
@@ -24,55 +24,57 @@ function getComputerChoice() {
         return 'scissors'.toLowerCase();
     }
 }
-
+/*
 let playerSelection = prompt('Make your choice: rock, paper, or scissors.').toLowerCase();
 let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection)); */
 
 //Player's Choice........................................
 
 function playRound (playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
-        return `Tie! You both picked ${playerSelection}`
+        return `Tie! You both picked ${playerSelection} Tie Score: ${++tieScore}`;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You win! Rock beats scissors.';
+        return `You win! Rock beats scissors. Player's Score: ${++playerScore}`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You win! Paper beats rock.';
+        return `You win! Paper beats rock. Player's Score: ${++playerScore}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You win! Scissors beats paper.';
+        return `You win! Scissors beats paper. Player's Score: ${++playerScore}`;
     } else {  
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        return `You lose! ${computerSelection} beats ${playerSelection}. Computer's Score: ${++compScore}`;
     }
 } 
 
-
-function winner() {
-    playRound(playerSelection, computerSelection)
-    if (playerSelection > computerSelection) {
+/*
+function winner(playerSelection, computerSelection) {
+    playRound();
+    if (playerSelection < computerSelection) {
         return `player scored! ${++playerScore}`;
     }
-    else if (computerSelection > playerSelection) {
+    else if (computerSelection < playerSelection) {
         return `comp scored! ${++compScore}`;
     }
     else {
-        return "Seems to be a tie";
+        return `Looks like a tie! ${++tieScore}`;
     }
-}
+} */
 let playerScore = 0;
 let compScore = 0;
-console.log(winner(playerSelection, computerSelection));
+let tieScore = 0;
+
 //GAME.......................................................
 
 function game() {
 
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i < 6; i++) {
         console.log(`Round number: ${i}`);
         let playerSelection = prompt('Make your choice: rock, paper, or scissors.').toLowerCase();
         let computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
-        playRound(playerSelection, computerSelection);  
-        winner(playerSelection, computerSelection); 
+    
+    
+        
     }
 }
 console.log(game());
